d4855cdc15d0b4130e5340f4cfea0d811ebbae18
kamal <shivamkamal.9936@gmail.com>
2025-11-02 20:19:40 +0530

    loading page

--- src/landing_page/OpenAccount.js
+++ src/landing_page/OpenAccount.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function OpenAccount() {
-    return ( 
-        <h1>OpenAccount</h1>
-     );
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <h1>Open a Zerodha Account</h1>
+        <p>
+          Modern platform and app, &#8377;0 inventments, and flat &#8377;20
+          intraday and F&O trades.{" "}
+        </p>
+        <button
+          className="p-3 btn btn-primary fs-5 mb-5"
+          style={{ width: "24%", margin: "0 auto" }}
+        >
+          Sing Up now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default OpenAccount;
--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Navbar from '../Navbar';
-import CreateTicket from './CreateTicket';
-import Hero from './hero';
+import React from "react";
+import Navbar from "../Navbar";
+import CreateTicket from "./CreateTicket";
+import Hero from "./Hero";
+import Footer from "../Footer";
 
 function SupportPage() {
-    return ( 
-        <>
-           <Navbar/>
-           <CreateTicket/>
-           <Hero/>
-           <Footer/>
-        </>
-     );
+  return (
+    <>
+      <Navbar />
+      <CreateTicket />
+      <Hero />
+      <Footer />
+    </>
+  );
 }
 
 export default SupportPage;
