5006622dbb3c3a9e1ef4d623f2db61f70a6d7f84
kamal <shivamkamal.9936@gmail.com>
2026-02-11 18:46:40 +0530

    Fixed frontend folder issue

--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "../Navbar";
+
 import CreateTicket from "./CreateTicket";
 import Hero from "./Hero";
-import Footer from "../Footer";
+
 
 function SupportPage() {
   return (
